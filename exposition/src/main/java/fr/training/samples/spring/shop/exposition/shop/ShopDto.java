1872d7a8671727e17aa5be014e1e15b385f34eb1
nilouis <nico.louise@gmail.com>
2020-12-09 09:49:45 +0100

    sauvegarde

---
+++ exposition/src/main/java/fr/training/samples/spring/shop/exposition/shop/ShopDto.java
@@ -0,0 +1,10 @@
+package fr.training.samples.spring.shop.exposition.shop;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ShopDto {
+
+    @JsonProperty
+    String name;
+
+}
